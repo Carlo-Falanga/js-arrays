501b578c18987782066f2780d7d65a614af78bb7
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-04 14:55:05 +0100

    complete 3rd bonus exercise

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -7,7 +7,6 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-
 const longNames = [];
 for (let i = 0; i < teachers.length; i++) {
   const longName = teachers[i];
@@ -18,6 +17,8 @@ for (let i = 0; i < teachers.length; i++) {
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const edRemove = teachers.splice(teachers.indexOf('Ed'), 1)
+console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
